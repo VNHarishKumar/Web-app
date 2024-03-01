5a8a7981e0395684da00a7e36fb362f8c3367f31
VNHarishKumar <72001980+VNHarishKumar@users.noreply.github.com>
2024-03-01 13:29:08 -0500

    Update dbloader.js

--- app/dbloader/dbloader.js
+++ app/dbloader/dbloader.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize({
-    dialect: 'mysql', // Specify the database dialect you're using (e.g., 'mysql', 'postgres', 'sqlite', etc.)
+    dialect: 'mysql', 
     host: process.env.MYSQL_HOST,
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
