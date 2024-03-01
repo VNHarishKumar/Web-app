4aa5fe719de88ad0d3c34489129cc55b7e74fec7
VNHarishKumar <72001980+VNHarishKumar@users.noreply.github.com>
2024-03-01 13:30:57 -0500

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,37 +7,15 @@ import * as auth from './../authenticate/authenticate.js';
 
 
 const route = (app) => {
-    // public route
-    // const statsmw = getStatsd();
-    // app.use(statsmw);
+   
     app.use(getStatsd());
-
-    // app.all('/', (req, res) => {
-    //     res.redirect('/v1/assignments');
-    // });
-
-
-    // app.get('/', (req, res) => {
-    //     res.redirect('/v1/assignments');
-    // });
-    
     app.get('/', (req, res) => {
         res.status(200).send(); // Send a 200 OK response
     });
     app.use('/healthz',assign1);
-    
-    // app.use('/',assign1);
     app.use('/v1/assignments',assign3);
-    // app.use('/v1/assignments', (req, res, next) => {
-    //     // Set the "WWW-Authenticate" header for Basic Authentication
-    //     res.set('WWW-Authenticate', 'Basic realm="Secure Area"');
-    //     auth.authorization(req,res);
-    // });
-    
     
     app.use('*',assign2.checkOtherMethod);
-   
-    // app.use(endStatsd());
 
 }
 
